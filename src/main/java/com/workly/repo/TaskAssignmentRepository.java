f7f684eaaee8d0cdae4e991b27c88c4a42608715
Pavankolisetty <kolisettypavan2003@gmail.com>
2026-04-02 13:44:37 +0530

    patch work: graph

--- src/main/java/com/workly/repo/TaskAssignmentRepository.java
+++ src/main/java/com/workly/repo/TaskAssignmentRepository.java
@@ -1,16 +1,19 @@
 package com.workly.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.workly.entity.TaskAssignment;
 import com.workly.entity.TaskStatus;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskAssignmentRepository extends JpaRepository<TaskAssignment, Long> {
     List<TaskAssignment> findByEmployeeEmpId(String empId);
     Optional<TaskAssignment> findByIdAndEmployeeEmpId(Long id, String empId);
     List<TaskAssignment> findByEmployeeEmpIdAndStatusNot(String empId, TaskStatus status);
     List<TaskAssignment> findByAssignedByOrderByAssignedAtDesc(String assignedBy);
+    @EntityGraph(attributePaths = {"task", "employee", "progressHistory"})
+    List<TaskAssignment> findAllByOrderByAssignedAtDesc();
     long countByEmployeeEmpId(String empId);
     long countByEmployeeEmpIdAndStatus(String empId, TaskStatus status);
     
